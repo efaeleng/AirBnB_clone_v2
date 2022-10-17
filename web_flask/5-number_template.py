da23f35c31e32095ee2aaa9f8ad2207df5e1c62b
Efa-Iwa <gistend@gmail.com>
2022-10-16 21:41:35 -0700

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""starts a flask web app"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """"returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """prints C is <text> depending on text given"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """prints Python followed by text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """prints n is a number if n is a number"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def templates(n):
+    """calls up a HTML page if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
